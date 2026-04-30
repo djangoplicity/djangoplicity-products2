7f449f7241b9b6f30d6901c758241b98710c4b6f
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-29 19:55:17 -0500

    Feature/Add new pdf2up resource

--- djangoplicity/products2/models.py
+++ djangoplicity/products2/models.py
@@ -914,6 +914,7 @@ class TheScope( ArchiveModel, StandardArchiveInfo, PhysicalInfo, PrintInfo ):
     class Archive( StandardArchiveInfo.Archive ):
         pdf = ResourceManager( type=types.PDFType )
         pdfsm = ResourceManager( type=types.PDFType, verbose_name=_( 'PDF File (Small)' ) )
+        pdf2up = ResourceManager( type=types.PDFType, verbose_name=_('PDF for Reading (Two-Page Spread)') )
         epub = ResourceManager(type=types.EPUBType)
         original = ImageResourceManager( verbose_name=_( 'Fullsize (RGB)' ), type=types.OriginalImageType )
         large = ImageResourceManager( derived='original', type=types.LargeJpegType )
